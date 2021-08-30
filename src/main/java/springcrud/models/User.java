668ac5f19ee21d8cc1a5475cc1801db7c447082c
Iw <iwor@list.ru>
2021-08-30 15:40:02 +0300

    overrode equals & hashCode in User

--- src/main/java/springcrud/models/User.java
+++ src/main/java/springcrud/models/User.java
@@ -1,6 +1,7 @@
 package springcrud.models;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name="users_crud")
@@ -42,6 +43,22 @@ public class User {
     }
     public void setYearOfBirth(int yearOfBirth) {
         this.yearOfBirth = yearOfBirth;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id
+                && yearOfBirth == user.yearOfBirth
+                && Objects.equals(name, user.name)
+                && Objects.equals(surname, user.surname);
+    }
+    
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname, yearOfBirth);
     }
     
     @Override
